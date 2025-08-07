956bbb2d11744c9a6f4eb1f106c94010da66227f
shreyas-hurakadli <shreyashurakadli23@gmail.com>
2025-08-07 19:25:38 +0530

    Made changes to Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.example.spend"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.spend"
         minSdk = 29
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
