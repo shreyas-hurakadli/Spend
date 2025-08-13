01f578533b3410dab5cf3dfa727ec2144bb7b3e6
shreyas-hurakadli <shreyashurakadli23@gmail.com>
2025-08-13 14:48:17 +0530

    Deleted manual DI files

--- app/src/main/java/com/example/spend/data/room/DatabaseRepositoryContainer.kt
+++
@@ -1,9 +0,0 @@
-package com.example.spend.data.room
-
-import android.content.Context
-
-class DatabaseRepositoryContainer(context: Context): RepositoryContainer {
-    override val databaseRepository: EntryRepository by lazy {
-        DefaultRepository(EntryDatabase.getDatabase(context).entryDao())
-    }
-}
--- app/src/main/java/com/example/spend/data/room/RepositoryContainer.kt
+++
@@ -1,5 +0,0 @@
-package com.example.spend.data.room
-
-interface RepositoryContainer {
-    val databaseRepository: EntryRepository
-}
