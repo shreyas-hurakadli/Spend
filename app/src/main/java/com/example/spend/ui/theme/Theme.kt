22bb1028b616366ee87e21e0f66c28315f645269
shreyas-hurakadli <shreyashurakadli23@gmail.com>
2025-08-17 16:13:24 +0530

    Changed the app theme

--- app/src/main/java/com/example/spend/ui/theme/Theme.kt
+++ app/src/main/java/com/example/spend/ui/theme/Theme.kt
@@ -1,28 +1,40 @@
 package com.example.spend.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 private val LightColorScheme = lightColorScheme(
-    primary = Color(0xFF8C6EFF),
-    secondary = Color(0xFFFBD472),
-    tertiary = Color(0xFFFEFEFE),
-    onPrimary = Color(0xFFFEFEFE),
-    onSecondary = Color.Black,
-    background = Color(0xFFFEFEFE),
-    onBackground = Color.DarkGray
+    primary = Color(0xFF353839),
+    secondary = Color(0xFF6200EE),
+    tertiary = Color(0xFFF5F5F5),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color(0xFF303030),
+    background = Color.White,
+    onBackground = Color.Black
+)
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Color(0xFF353839),
+    secondary = Color(0xFF6200EE),
+    tertiary = Color(0xFFF5F5F5),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color(0xFF303030),
+    background = Color.White,
+    onBackground = Color.Black
 )
 
 @Composable
 fun SpendTheme(
     content: @Composable () -> Unit
 ) {
-    val colorScheme = LightColorScheme
-
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = if (isSystemInDarkTheme()) DarkColorScheme else LightColorScheme,
         typography = Typography,
         content = content
     )
