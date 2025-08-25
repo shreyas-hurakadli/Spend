e5a0e4481dbd7aa5cc3e1fe1faa3e8d6cb07413f
shreyas-hurakadli <shreyashurakadli23@gmail.com>
2025-08-25 15:30:22 +0530

    Added animations

--- app/src/main/java/com/example/spend/ui/navigation/NavigationManager.kt
+++ app/src/main/java/com/example/spend/ui/navigation/NavigationManager.kt
@@ -1,7 +1,8 @@
 package com.example.spend.ui.navigation
 
-import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -11,98 +12,47 @@ import com.example.spend.ui.screen.EntryScreen
 import com.example.spend.ui.screen.ExpensesScreen
 import com.example.spend.ui.screen.HomeScreen
 
+private const val durationMillis = 300
 @Composable
 fun NavigationManager(navHostController: NavHostController) {
     NavHost(
         navController = navHostController,
         startDestination = Routes.HomeScreen,
+        enterTransition = {
+            slideInHorizontally(
+                initialOffsetX = { fullWidth -> fullWidth },
+                animationSpec = tween(durationMillis)
+            )
+        },
+        exitTransition = {
+            slideOutHorizontally(
+                targetOffsetX = { fullWidth -> -fullWidth },
+                animationSpec = tween(durationMillis)
+            )
+        },
+        popEnterTransition = {
+            slideInHorizontally(
+                initialOffsetX = { fullWidth -> -fullWidth },
+                animationSpec = tween(durationMillis)
+            )
+        },
+        popExitTransition = {
+            slideOutHorizontally(
+                targetOffsetX = { fullWidth -> fullWidth },
+                animationSpec = tween(durationMillis)
+            )
+        }
     ) {
-        composable<Routes.HomeScreen>(
-            enterTransition = {
-                slideIntoContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
-                    animationSpec = tween(durationMillis = 500)
-                )
-            },
-            popEnterTransition = {
-                slideIntoContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
-                    animationSpec = tween(durationMillis = 500)
-                )
-            },
-            exitTransition = {
-                slideOutOfContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Left,
-                    animationSpec = tween(durationMillis = 500)
-                )
-            }
-        ) {
+        composable<Routes.HomeScreen> {
             HomeScreen(navHostController = navHostController)
         }
-        composable<Routes.ExpensesScreen>(
-            enterTransition = {
-                slideIntoContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Left,
-                    animationSpec = tween(durationMillis = 500)
-                )
-            },
-            popEnterTransition = {
-                slideIntoContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Left,
-                    animationSpec = tween(durationMillis = 500)
-                )
-            },
-            exitTransition = {
-                slideOutOfContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
-                    animationSpec = tween(durationMillis = 500)
-                )
-            }
-        ) {
+        composable<Routes.ExpensesScreen> {
             ExpensesScreen(navHostController = navHostController)
         }
-        composable<Routes.AddScreen>(
-            enterTransition = {
-                slideIntoContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Down,
-                    animationSpec = tween(durationMillis = 500)
-                )
-            },
-            popEnterTransition = {
-                slideIntoContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Down,
-                    animationSpec = tween(durationMillis = 500)
-                )
-            },
-            exitTransition = {
-                slideOutOfContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Up,
-                    animationSpec = tween(durationMillis = 500)
-                )
-            }
-        ) {
+        composable<Routes.AddScreen> {
             AddScreen(navHostController = navHostController)
         }
-        composable<Routes.EntryScreen>(
-            enterTransition = {
-                slideIntoContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Up,
-                    animationSpec = tween(durationMillis = 500)
-                )
-            },
-            popEnterTransition = {
-                slideIntoContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Up,
-                    animationSpec = tween(durationMillis = 500)
-                )
-            },
-            exitTransition = {
-                slideOutOfContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Down,
-                    animationSpec = tween(durationMillis = 500)
-                )
-            }
-        ) {
+        composable<Routes.EntryScreen> {
             EntryScreen(navHostController = navHostController)
         }
     }
