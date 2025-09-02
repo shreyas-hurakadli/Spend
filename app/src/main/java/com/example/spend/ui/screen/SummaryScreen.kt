f4ea3b67cb6ee64dd586a0cbe2bdf2912f9b1f59
shreyas-hurakadli <shreyashurakadli23@gmail.com>
2025-09-02 21:37:20 +0530

    Optimized imports

--- app/src/main/java/com/example/spend/ui/screen/SummaryScreen.kt
+++ app/src/main/java/com/example/spend/ui/screen/SummaryScreen.kt
@@ -1,6 +1,5 @@
 package com.example.spend.ui.screen
 
-import android.util.Log
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -15,7 +14,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
