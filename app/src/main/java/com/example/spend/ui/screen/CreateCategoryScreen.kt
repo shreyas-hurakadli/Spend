8614698aba8bb4224801a67faf93baf36e469892
shreyas-hurakadli <shreyashurakadli23@gmail.com>
2025-10-16 18:14:04 +0530

    Optimized imports

--- app/src/main/java/com/example/spend/ui/screen/CreateCategoryScreen.kt
+++ app/src/main/java/com/example/spend/ui/screen/CreateCategoryScreen.kt
@@ -1,6 +1,5 @@
 package com.example.spend.ui.screen
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -40,7 +39,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
