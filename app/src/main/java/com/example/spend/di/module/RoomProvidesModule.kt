7082d1382994932f814ab9d33dbfda1b20528aa5
shreyas-hurakadli <shreyashurakadli23@gmail.com>
2025-10-23 16:13:03 +0530

    Added default accounts and categories

--- app/src/main/java/com/example/spend/di/module/RoomProvidesModule.kt
+++ app/src/main/java/com/example/spend/di/module/RoomProvidesModule.kt
@@ -23,15 +23,47 @@ object RoomProvidesModule {
         override fun onCreate(db: SupportSQLiteDatabase) {
             super.onCreate(db)
             db.execSQL("INSERT INTO accounts (name, balance) VALUES ('All', 0.0)")
+            db.execSQL("INSERT INTO accounts (name, balance) VALUES ('Cash', 0.0)")
+            db.execSQL("INSERT INTO accounts (name, balance) VALUES ('Card', 0.0)")
+            db.execSQL("INSERT INTO accounts (name, balance) VALUES ('Savings', 0.0)")
+
             db.execSQL("INSERT INTO categories (name, is_expense, color) VALUES ('All', 0, -1)")
             db.execSQL("INSERT INTO categories (name, is_expense, color) VALUES ('All', 1, -1)")
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Awards', 0, -8921737, 'award')");
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Coupons', 0, -5323057, 'label')");
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Deposit', 0, -3429685, 'coin')");
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Salary', 0, -19641, 'coin')");
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Car', 1, -8921737, 'car')");
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Entertainment', 1, -5323057, 'movie')");
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Food', 1, -3429685, 'food')");
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Health', 1, -19641, 'hospital')");
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Home', 1, -5005643, 'home')");
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Shopping', 1, -38559, 'groceries')");
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Sports', 1, -38559, 'game')");
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Transportation', 1, -7357297, 'bus')");
         }
 
         override fun onDestructiveMigration(db: SupportSQLiteDatabase) {
             super.onDestructiveMigration(db)
             db.execSQL("INSERT INTO accounts (name, balance) VALUES ('All', 0.0)")
+            db.execSQL("INSERT INTO accounts (name, balance) VALUES ('Cash', 0.0)")
+            db.execSQL("INSERT INTO accounts (name, balance) VALUES ('Card', 0.0)")
+            db.execSQL("INSERT INTO accounts (name, balance) VALUES ('Savings', 0.0)")
+
             db.execSQL("INSERT INTO categories (name, is_expense, color) VALUES ('All', 0, -1)")
             db.execSQL("INSERT INTO categories (name, is_expense, color) VALUES ('All', 1, -1)")
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Awards', 0, -8921737, 'award')");
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Coupons', 0, -5323057, 'label')");
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Deposit', 0, -3429685, 'coin')");
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Salary', 0, -19641, 'coin')");
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Car', 1, -8921737, 'car')");
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Entertainment', 1, -5323057, 'movie')");
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Food', 1, -3429685, 'food')");
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Health', 1, -19641, 'hospital')");
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Home', 1, -5005643, 'home')");
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Shopping', 1, -38559, 'groceries')");
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Sports', 1, -38559, 'game')");
+            db.execSQL("INSERT INTO categories (name, is_expense, color, icon) VALUES ('Transportation', 1, -7357297, 'bus')");
         }
     }
     @Provides
