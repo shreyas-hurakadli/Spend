306248e6b63f8b2cbbf1f4fb10494f83d9a08e03
shreyas-hurakadli <shreyashurakadli23@gmail.com>
2025-10-25 17:13:54 +0530

    Made Account Screen better

--- app/src/main/java/com/example/spend/ui/screen/AddAccountScreen.kt
+++ app/src/main/java/com/example/spend/ui/screen/AddAccountScreen.kt
@@ -1,18 +1,23 @@
 package com.example.spend.ui.screen
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Done
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -24,6 +29,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -33,6 +41,7 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.spend.R
+import com.example.spend.getLocalCurrencySymbol
 import com.example.spend.ui.viewmodel.AddAccountViewModel
 import com.example.spend.validateCurrency
 
@@ -41,9 +50,10 @@ fun AddAccountScreen(
     navHostController: NavHostController,
     viewModel: AddAccountViewModel = hiltViewModel()
 ) {
-    var balance by remember { mutableStateOf("") }
-    var name by remember { mutableStateOf("") }
-    val keyboardController = LocalSoftwareKeyboardController.current
+    val uiState by viewModel.uiState.collectAsState()
+    val balance by viewModel.balance.collectAsState()
+    val focusManager = LocalFocusManager.current
+    val interactionSource = remember { MutableInteractionSource() }
 
     Scaffold(
         topBar = {
@@ -58,18 +68,23 @@ fun AddAccountScreen(
             contentAlignment = Alignment.Center,
             modifier = Modifier
                 .padding(innerPadding)
-                .fillMaxSize()
+                .clickable(
+                    enabled = true,
+                    interactionSource = interactionSource,
+                    indication = null,
+                    onClick = { focusManager.clearFocus() }
+                )
         ) {
             Column(
                 verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxSize()
             ) {
                 OutlinedTextField(
-                    value = name,
-                    onValueChange = { value ->
-                        name = value.trim()
-                    },
+                    value = uiState.name,
+                    onValueChange = { viewModel.updateName(it) },
                     label = {
                         Text(
                             text = stringResource(R.string.account),
@@ -77,6 +92,8 @@ fun AddAccountScreen(
                             color = MaterialTheme.colorScheme.onBackground
                         )
                     },
+                    singleLine = true,
+                    shape = RoundedCornerShape(24.dp),
                     textStyle = TextStyle(
                         color = MaterialTheme.colorScheme.onBackground,
                         fontSize = 16.sp
@@ -84,47 +101,78 @@ fun AddAccountScreen(
                     keyboardOptions = KeyboardOptions(
                         keyboardType = KeyboardType.Text
                     ),
+                    modifier = Modifier.fillMaxWidth()
                 )
                 Spacer(Modifier.height(16.dp))
-                OutlinedTextField(
-                    value = balance,
-                    onValueChange = { value ->
-                        if (value == "")
-                            balance = ""
-                        else if (validateCurrency(value))
-                            balance = value
-                    },
-                    label = {
-                        Text(
-                            text = stringResource(R.string.amount),
-                            style = MaterialTheme.typography.labelSmall,
-                            color = MaterialTheme.colorScheme.onBackground
-                        )
-                    },
-                    textStyle = TextStyle(
-                        color = MaterialTheme.colorScheme.onBackground,
-                        fontSize = 16.sp
-                    ),
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Number
-                    ),
-                )
-                Spacer(Modifier.height(16.dp))
-                if (name != "" && balance != "") {
-                    FloatingActionButton(
-                        onClick = {
-                            viewModel.updateName(name)
-                            viewModel.updateBalance(balance.toDouble())
-                            viewModel.insertData()
-                            keyboardController?.hide()
-                            name = ""
-                            balance = ""
-                        }
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    OutlinedTextField(
+                        value = "INR",
+                        onValueChange = {},
+                        trailingIcon = {
+                            Text(
+                                text = getLocalCurrencySymbol() ?: "$",
+                                color = Color.Black
+                            )
+                        },
+                        label = {},
+                        readOnly = true,
+                        singleLine = true,
+                        shape = RoundedCornerShape(24.dp),
+                        textStyle = TextStyle(
+                            color = MaterialTheme.colorScheme.onBackground,
+                            fontSize = 16.sp
+                        ),
+                        modifier = Modifier.weight(0.3f)
+                    )
+                    Spacer(Modifier.weight(0.1f))
+                    OutlinedTextField(
+                        value = balance,
+                        onValueChange = { viewModel.updateBalance(it) },
+                        label = {
+                            Text(
+                                text = stringResource(R.string.initial_amount),
+                                style = MaterialTheme.typography.labelSmall,
+                                color = MaterialTheme.colorScheme.onBackground
+                            )
+                        },
+                        singleLine = true,
+                        shape = RoundedCornerShape(24.dp),
+                        textStyle = TextStyle(
+                            color = MaterialTheme.colorScheme.onBackground,
+                            fontSize = 16.sp
+                        ),
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Number
+                        ),
+                        modifier = Modifier.weight(0.6f)
+                    )
+                }
+                Spacer(Modifier.weight(1f))
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    OutlinedButton(
+                        onClick = { viewModel.clear() },
+                        modifier = Modifier.weight(1f)
                     ) {
-                        Icon(
-                            imageVector = Icons.Filled.Done,
-                            contentDescription = stringResource(R.string.done),
-                        )
+                        Text("Cancel")
+                    }
+                    Spacer(Modifier.width(8.dp))
+                    OutlinedButton(
+                        onClick = { viewModel.save(balance) },
+                        colors = ButtonDefaults.outlinedButtonColors(
+                            containerColor = MaterialTheme.colorScheme.primary,
+                            contentColor = MaterialTheme.colorScheme.onPrimary
+                        ),
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text("Create")
                     }
                 }
             }
--- app/src/main/java/com/example/spend/ui/viewmodel/AddAccountViewModel.kt
+++ app/src/main/java/com/example/spend/ui/viewmodel/AddAccountViewModel.kt
@@ -1,35 +1,69 @@
 package com.example.spend.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.spend.data.room.account.Account
 import com.example.spend.data.room.account.AccountRepository
+import com.example.spend.validateCurrency
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jakarta.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
+
+private const val durationMillis = 1_000L
 
 @HiltViewModel
 class AddAccountViewModel @Inject constructor(
     private val defaultAccountRepository: AccountRepository
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(Account())
+    val uiState = _uiState.asStateFlow()
+
+    private var _balance = MutableStateFlow("")
+    val balance = _balance.asStateFlow()
+
+    private val allAccount = defaultAccountRepository.getFirstAccount()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(durationMillis),
+            initialValue = Account()
+        )
+
     fun updateName(name: String) {
         _uiState.value = _uiState.value.copy(name = name)
     }
 
-    fun updateBalance(balance: Double) {
-        _uiState.value = _uiState.value.copy(balance = balance)
+    fun updateBalance(value: String) {
+        if (validateCurrency(value)) {
+            _balance.value = value
+        }
     }
 
-    private fun clear() {
+    fun clear() {
         _uiState.value = _uiState.value.copy(name = "")
+        _balance.value = ""
     }
 
-    fun insertData() {
+    fun save(balance: String) {
+        if (validateCurrency(balance)) {
+            _uiState.value = _uiState.value.copy(balance = balance.toDouble())
+        }
         viewModelScope.launch {
-            defaultAccountRepository.insert(_uiState.value)
+            if (allAccount != Account()) {
+                defaultAccountRepository.insert(_uiState.value)
+                defaultAccountRepository.update(
+                    account = allAccount.value.copy(
+                        balance = balance.toDouble() + allAccount.value.balance
+                    )
+                )
+            }
             clear()
         }
     }
