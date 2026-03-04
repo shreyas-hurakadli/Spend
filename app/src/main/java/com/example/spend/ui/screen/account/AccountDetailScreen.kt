96a12218ce86f7c52d4d41df07b2da0bf3231d76
shreyas-hurakadli <shreyashurakadli23@gmail.com>
2026-03-04 16:06:17 +0530

    Changed the icon tint

--- app/src/main/java/com/example/spend/ui/screen/account/AccountDetailScreen.kt
+++ app/src/main/java/com/example/spend/ui/screen/account/AccountDetailScreen.kt
@@ -30,6 +30,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
