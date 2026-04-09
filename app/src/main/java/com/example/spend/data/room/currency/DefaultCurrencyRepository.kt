25e7a91b3f8a5a441a6fbf4708190eed0b457033
shreyas-hurakadli <shreyashurakadli23@gmail.com>
2026-04-09 18:47:57 +0530

    Removed unnecessary coroutine dispatcher

--- app/src/main/java/com/example/spend/data/room/currency/DefaultCurrencyRepository.kt
+++ app/src/main/java/com/example/spend/data/room/currency/DefaultCurrencyRepository.kt
@@ -1,28 +1,18 @@
 package com.example.spend.data.room.currency
 
 import jakarta.inject.Inject
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.withContext
 
 class DefaultCurrencyRepository @Inject constructor(
     private val dao: CurrencyDao,
-    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : CurrencyRepository {
-    override suspend fun insert(currency: Currency) = withContext(context = dispatcher) {
-        dao.insert(currency)
-    }
+    override suspend fun insert(currency: Currency) = dao.insert(currency)
 
-    override suspend fun update(currency: Currency) = withContext(context = dispatcher) {
-        dao.update(currency)
-    }
+    override suspend fun update(currency: Currency) = dao.update(currency)
 
-    override suspend fun delete(currency: Currency) = withContext(context = dispatcher) {
-        dao.delete(currency)
-    }
+    override suspend fun delete(currency: Currency) = dao.delete(currency)
 
-    override suspend fun deleteAll() = withContext(context = dispatcher) {
+    override suspend fun deleteAll() {
         dao.deleteAll()
         dao.resetAutoIncrement()
     }
