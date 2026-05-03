9de2a99c6ea4121fa3caea56908520ce8f1f06e6
shreyas-hurakadli <shreyashurakadli23@gmail.com>
2026-05-03 14:59:58 +0530

    Removed signing config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,6 @@ android {
         release {
             isMinifyEnabled = true
             isShrinkResources = true
-            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
